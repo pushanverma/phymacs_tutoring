d62969ed8bd4b63d1bebc1eac20cc12a5d28cbdf
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2026-08-26 22:39:31 -0400

    Wrapping around HelmetProvider

--- phymacs/src/main.jsx
+++ phymacs/src/main.jsx
@@ -6,13 +6,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { store } from "./app/store";
 import App from "./App.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>
+    <HelmetProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </HelmetProvider>
+  </StrictMode>,
 );
--- phymacs/src/pages/BecomeTutor.jsx
+++ phymacs/src/pages/BecomeTutor.jsx
@@ -15,7 +15,10 @@ const BecomeTutor = () => (
       />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://phymacstutoring.com/become-a-tutor" />
+      <meta
+        property="og:url"
+        content="https://phymacstutoring.com/become-a-tutor"
+      />
       <meta
         property="og:title"
         content=" Become a Tutor | Join PhyMacs Tutoring "
@@ -25,7 +28,10 @@ const BecomeTutor = () => (
         content="Join PhyMacs Tutoring as a tutor and help students achieve their academic goals. Learn about becoming a tutor, sharing your expertise, and growing with our tutoring community."
       />
       <meta property="og:site_name" content="PhyMacs Tutoring" />
-      <meta property="og:image" content="https://www.phymacstutoring.com/og-image.jpg" />
+      <meta
+        property="og:image"
+        content="https://www.phymacstutoring.com/og-image.jpg"
+      />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <link rel="canonical" href="https://www.phymacstutoring.com" />
